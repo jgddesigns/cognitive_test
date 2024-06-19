af473b9f911dec739aa2597986b1fc71e11dcfea
Jason Dunn <jdunn7008@gmail.com>
2024-06-19 09:45:49 -0700

    continuing test 5

--- cognitive_test/src/tests/MemoryScanning.tsx
+++ cognitive_test/src/tests/MemoryScanning.tsx
@@ -14,62 +14,141 @@ export default function MemoryScanning (props: any) {
     const [AnswerCount, setAnswerCount] = React.useState(0)
     const [IntervalTime, setIntervalTime] = React.useState(0)
     const [ShowPrompt, setShowPrompt] = React.useState(false)
-
+    const [ShowCompare, setShowCompare] = React.useState(false)
+    const [CompareMessage, setCompareMessage] = React.useState(false)
+    const [CompareNumbers, setCompareNumbers] = React.useState(false)
+    const [ShowButtons, setShowButtons] = React.useState(false)  
+    const [CompareDigits, setCompareDigits] = React.useState(-1)
     const [DigitList, setDigitList] = React.useState([])
-    const [CurrentDigit, setCurrentDigit] = React.useState("")
+    const [StaticList, setStaticList] = React.useState([])
+    const [CurrentDigit, setCurrentDigit] = React.useState("4")
     const [CompareList, setCompareList] = React.useState([])
+    const [CompareString, setCompareString] = React.useState("ftstsg")
+    const [Digits, setDigits] = React.useState(-1)
 
-    // const number_class = ["text-2xl bold", "text-2xl bold text-green-400"]
-    // const [NumberClass, setNumberClass] = React.useState(number_class[0])
 
 
 
     useEffect(() => {
-        var count = 1
-        var digits = 3
-        while(IntervalTime > 0){
+
+        var temp_arr: any = DigitList
+        var compare_arr: any = CompareList
+        while(Digits >= 0){
+
             const timeoutId = setTimeout(() => {
-                setIntervalTime(IntervalTime-.5)
-                count = IntervalTime
-                if(count <= .5){
-                    set_interval()
-                    if(digits %2 == 0){
-                        setCurrentDigit(DigitList[digits]) 
-                    }else{
-                        setCurrentDigit("")
-                    }
-                    digits-=1
+
+                if(Digits % 2 != 0){
+                    setCurrentDigit(DigitList[DigitList.length-1]) 
+                    temp_arr.pop(0)
+                    setDigitList(temp_arr)
+                }else{
+                    setCurrentDigit("")
                 }
-            }, 500 )
+
+                setDigits(Digits - 1)
+                if(Digits < 1){
+                    setCompareString("")
+                    setShowPrompt(false)
+                    setShowCompare(true)
+                    setCompareMessage(true)
+                    setCompareDigits(4)
+                }
+                console.log(StaticList)
+
+            }, 2500 )
 
             return () => clearTimeout(timeoutId)
         }
 
-    }, [IntervalTime])
+        while(CompareDigits >= 0 && (CompareMessage || CompareNumbers)){
+            const timeoutId = setTimeout(() => {
+                if(CompareMessage){
+                    if(CompareDigits == 4){
+                        setCompareString("Compare the digits to the next 18 numbers.")
+                    }
+                    if(CompareDigits == 3){
+                        setCompareString("")
+                    }
+                    if(CompareDigits == 2){
+                        create_string()
+                    }
+                    if(CompareDigits == 0){
+                        setCompareString("")
+                        setCompareNumbers(true)
+                        setCompareMessage(false)
+                        setCompareDigits(36)
+                        console.log("12345")
+                    }
+                }
+
+                if(CompareNumbers){
+                    compare_arr = CompareList
+                    console.log("\n\ncompare list")
+                    console.log(compare_arr)
+                    console.log(StaticList)
+                    if(CompareDigits %2 != 0){
+                        setShowButtons(true)
+                        setShowCompare(true)  
+                        setCompareString(CompareList[CompareList.length-1])
+                        compare_arr.pop(0)
+                        setCompareList(compare_arr)
+                    }else{
+                        setShowButtons(false)
+                        setShowCompare(false) 
+                        setCompareString("")      
+                    }
+
+                    if(CompareDigits == 0){
+                        setCompareNumbers(false)
+                        setEndTest(true)
+                    }
+
+                }
+                console.log(CompareDigits)
+
+                    CompareDigits == 0 ? setCompareDigits(36) :              setCompareDigits(CompareDigits - 1)
+
+
+
+            }, 2500 )
+
+            return () => clearTimeout(timeoutId)
+
+        }
+
+
+
+    }, [IntervalTime, Digits, DigitList, CompareDigits])
 
     //partially from chat gpt
     function create_list(){
         var temp_list: any = []
+        var static_list: any = []
 
         var i = 0
         while(i < 3){
-            temp_list.push(Math.floor(Math.random() * ((1000-1)+1)) + 1)
+            var num: any = Math.floor(Math.random() * ((1000-1)+1)) + 1
+            temp_list.push(num)
+            static_list.push(num)
             i++
         }
-        //setDigitList(temp_list)
-        setCurrentDigit(temp_list[0])
+
+        setCurrentDigit(temp_list[temp_list.length-1])
+        setStaticList(static_list)
         temp_list.pop(0)
         setDigitList(temp_list)
-        compare_list(temp_list)
+        compare_list(static_list)
     }
 
 
     function compare_list(list: any){
+        console.log("temp list set compare")
         console.log(list)
         var temp_list: any  = []
         temp_list = temp_list.concat(list)
+        console.log(temp_list)
 
-        var i = list.length
+        var i = temp_list.length
         while(i < 18){
             var num: any = Math.floor(Math.random() * ((1000-1)+1)) 
             if(!temp_list.includes(num)){
@@ -77,9 +156,10 @@ export default function MemoryScanning (props: any) {
                 i++
             }
         }
+        console.log(temp_list)
         temp_list = shuffle_list(temp_list, temp_list.length)
-        setCompareList(temp_list)
 
+        setCompareList(temp_list)
     }
 
     function shuffle_list(list: any, size: any){
@@ -96,66 +176,65 @@ export default function MemoryScanning (props: any) {
 
         console.log("new list")
         console.log(new_list)
+
+        setShowPrompt(true)
         return new_list
     }
 
 
+    function create_string(){
+        var temp_arr: any = CompareList
+        console.log("compare create string")
+        console.log(temp_arr)
+        console.log(CompareList)
+        var str: any = ""
+
+        for(var i=0; i<CompareList.length; i++){
+            str = str + " " + CompareList[i]
+        }
+
+        setCompareString(str)
+    }
 
 
-    // function get_prompt(){
-    //     PromptList.length < 1 ? setEndTest(true) : null
-    //     var temp_arr = PromptList
-    //     console.log(PromptList)
-    //     var pos = Math.floor(Math.random() * temp_arr.length)   
-    //     var curr_prompt = temp_arr[pos]
-    //     var spot = 0
-    //     for(var i = 0; i < prompt_list.length; i++){
-    //         if(prompt_list[i] == curr_prompt){
-    //             spot = i
-    //             break
-    //         }
-    //     }
-    //     spot < 10 ? setAnswer(true) : setAnswer(false)
-    //     setCurrentPrompt(curr_prompt)
-    //     temp_arr.splice(pos, 1)
-    //     setPromptList(temp_arr)
-    // }
-
-    function start_handler(){
-        create_list()
-
-        // shuffle_list(DigitList, DigitList.length)
-        // setTestStart(true)
-        setShowPrompt(true)
+    function yes_handler(){
+        // setYesCount(YesCount + 1)
+        answer_handler(true)
         // get_prompt()
     }
 
-    // function yes_handler(){
-    //     setYesCount(YesCount + 1)
-    //     answer_handler(true)
-    //     get_prompt()
-    // }
 
-    // function no_handler(){
-    //     setNoCount(NoCount + 1)
-    //     answer_handler(false)
-    //     get_prompt()
-    // }
+    function no_handler(){
+        // setNoCount(NoCount + 1)
+        answer_handler(false)
+        // get_prompt()
+    }
+
 
     function answer_handler(answer: any){
-        answer == Answer ? console.log("correct") : null
-        answer == Answer ? setAnswerCount(AnswerCount + 1) : null
-        setShowPrompt(false)
-        set_interval()
+        setCompareString("")
+        setShowButtons(false)
+        var temp_arr: any = StaticList
+        console.log(StaticList)
+        console.log(CompareString)
+        console.log(answer)
+        answer && temp_arr.includes(CompareString) || (!answer && !temp_arr.includes(CompareString)) ? setAnswerCount(AnswerCount + 1) : null
+
     }
+
+
+    function start_handler(){
+        create_list()
+        setDigits(4)
+        setTestStart(true)
+        setShowPrompt(true)
+    }
+
 
     function calculate_ratio(){
-        return Math.round((AnswerCount/20)*100)
+        return Math.round((AnswerCount/18)*100)
     }
 
-    function set_interval(){
-        setIntervalTime(2.5)
-    }
 
   return(
     <div>
@@ -163,39 +242,48 @@ export default function MemoryScanning (props: any) {
             TEST #5: MEMORY SCANNING TASK TEST
         </div>
         <div className="row mt-12 text-sky-400">
-            Three digits are presented singly at the rate of one every 2.5 seconds for the patient to remember. A series of 18 digits is then presented. For each, the patient must press Yes or No according to whether the digit is thought to be one of the three presented initially.
+            Three digits are presented singly at the rate of one every 2.5 seconds for the player to remember. A series of 18 digits is then presented. For each, the player must press Yes or No according to whether the digit is thought to be one of the three presented initially.
         </div>
         {!EndTest ?
             !TestStart ? 
                 <div className="mt-[200px]">              
                     <Button className="bg-blue-400 rounded px-10 h-12 text-white" onClick={start_handler}>
-                        Start
+                        Start   
                     </Button>  
-                    {/* <div>Digits: {DigitList}</div>  
-                    <div>Compare: {CompareList} </div>                              */}
                 </div>
             :   ShowPrompt ?
+
                     <div className="mt-[200px] grid grid-rows-2">
                         <span>
-                            {CurrentDigit}
+                           {CurrentDigit}
                         </span>
-                        {/* <div className="mt-12 grid grid-cols-2">
+
+                    </div>    
+                : ShowCompare ?
+
+                <div className="mt-[200px] grid grid-rows-2">
+                    <span>
+                       {CompareString}
+                    </span>
+                    {ShowButtons ?
+                        <div className="mt-12 grid grid-cols-2">
                             <Button className="bg-green-400 rounded px-10 h-12 text-white" onClick={yes_handler}>
                                 Yes
                             </Button>
                             <Button className="bg-red-400 rounded px-10 h-12 text-white" onClick={no_handler}>
                                 No
                             </Button>
-                        </div> */}
-                    </div>    
-                : null  
+                        </div>
+                    : null}
+                </div>    
+            : null
         :
             <div className="grid grid-rows-3 mt-[200px]"> 
                 <span className="mt-12">
                     Test is Over
                 </span> 
                 <span className="mt-12">
-                    {AnswerCount} answers correct out of 20. ({calculate_ratio()}%)
+                    {AnswerCount} answers correct out of 18. ({calculate_ratio()}%)
                 </span>
             </div>
         }
