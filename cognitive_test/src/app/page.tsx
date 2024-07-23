45addac6b1e461dc67e352f2c769546b9ac6c391
Jason Dunn <jdunn7008@gmail.com>
2024-07-23 14:19:53 -0700

    working on main page

--- cognitive_test/src/app/page.tsx
+++ cognitive_test/src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import './globals.css'
 import Image from 'next/image'
@@ -19,31 +20,178 @@ import Tests from '@/screens/Tests'
 export default function Home() {
 
   const main_class = ["flex min-h-screen flex-col items-center justify-between p-24"
-, "h-[100%] w-[100%] bg-blue-400 relative"]
+, "flex min-h-screen flex-col items-center justify-between p-24 h-[100%] w-[100%]  relative"]
   const [MainClass, setMainClass] = React.useState(main_class[0])
 
+  const header_text = ["Test Info", "Home"]
+  const [HeaderText, setHeaderText] = React.useState(header_text[0])
+  const link_class = ["text-blue-600", "text-gray-600 font-bold underline"]
+  const [LinkClass, setLinkClass] = React.useState(link_class[0])
+ 
+  const [ShowPopover, setShowPopover] = React.useState(false)
+  const [ShowTestInfo, setShowTestInfo] = React.useState(false)
+  const [ShowChoiceReaction, setShowChoiceReaction] = React.useState(false)
+  const [ShowDigitVigilance, setShowDigitVigilance] = React.useState(false)
+  const [ShowMemoryScanning, setShowMemoryScanning] = React.useState(false)
+  const [ShowMotorFunction, setShowMotorFunction] = React.useState(false)
+  const [ShowNumberVigilance, setShowNumberVigilance] = React.useState(false)
+  const [ShowPictureRecognition, setShowPictureRecognition] = React.useState(false)
+  const [ShowReactionTime, setShowReactionTime] = React.useState(false)
+  const [ShowVerbalLearning, setShowVerbalLearning] = React.useState(false)
+  const [ShowWordRecognition, setShowWordRecognition] = React.useState(false)
+  const [ShowWorkingMemory, setShowWorkingMemory,] = React.useState(false)
+
+
+  const [TestID, setTestID] = React.useState(-1)
+  const [TestTitle, setTestTitle] = React.useState("")
+  const [PopoverMessage, setPopoverMessage] = React.useState("")
+
+  function hide_popover(){
+    setShowPopover(false)
+    setMainClass(main_class[0])
+    setPopoverMessage("")
+    setTestTitle("")
+  }
+
+  function toggle_tests(){
+    clear_tests()
+    setShowTestInfo(!ShowTestInfo)
+    LinkClass == link_class[0] ? setLinkClass(link_class[1]) : setLinkClass(link_class[0])
+    HeaderText == header_text[0] ? setHeaderText(header_text[1]) : setHeaderText(header_text[0])
+  }
+
+  function clear_tests(){
+    var test_types = [        
+      setShowChoiceReaction, setShowDigitVigilance, setShowMemoryScanning, setShowMotorFunction, setShowNumberVigilance, setShowPictureRecognition,setShowReactionTime, setShowVerbalLearning, setShowWordRecognition,setShowWorkingMemory
+    ]
+
+    for(var i=0; i<test_types.length; i++){
+      test_types[i](false)
+    }
+  }
+
+  function take_test(){
+    setLinkClass(link_class[0])
+    setShowPopover(false)
+    setShowTestInfo(false)
+
+    clear_tests()
+
+
+    switch (TestID){
+      case 1:
+        setShowChoiceReaction(true)
+        break
+      case 2:
+        setShowDigitVigilance(true)
+        break
+      case 3:
+        setShowMemoryScanning(true)
+        break
+      case 4:
+        setShowMotorFunction(true)
+        break
+      case 5:
+        setShowNumberVigilance(true)
+        break
+      case 6:
+        setShowPictureRecognition(true)
+        break
+      case 7:
+        setShowReactionTime(true)
+        break
+      case 8:
+        setShowVerbalLearning(true)
+        break
+      case 9:
+        setShowWordRecognition(true)
+        break
+      case 10:
+        setShowWorkingMemory(true)
+        break
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg lg:flex">
+    <main className={MainClass}>
+      <div className="z-1 max-w-5xl w-full items-center font-mono text-lg grid grid-auto-rows">
+        <div>        
+          <span onClick={toggle_tests} className={LinkClass}>{HeaderText}</span>
+        </div>
 
-        {/* TEST AREA FOR DIFFERENT COMPONENTS (SHOWS ON MAIN PAGE) */}
-        {/* CURRENTLY SET UP FOR TEST DEBUGGINH. THIS PAGE WILL ONLY HAVE MAIN PAGE COMPONENT IN FINAL VERSION. */}
+        <div className="mt-24">
+          {ShowTestInfo ?
+            <Tests setMainClass={setMainClass} main_class={main_class} setShowPopover={setShowPopover} setPopoverMessage={setPopoverMessage} setTestTitle={setTestTitle} setTestID={setTestID}/>
+          : null}
 
+          {ShowChoiceReaction ?
+            <ChoiceReaction/>
+          : null}
 
-        {/* <Connect/> */}
-        {/* <VerbalLearning/> */}
-        {/* <ReactionTime/> */}
-        {/* <NumberVigilance/> */}
-        {/* {<ChoiceReaction/>} */}
-        {/* {<MemoryScanning/>} */}
-        {/* {<PictureRecognition/>} */}
-        {/* {<DigitVigilance/>} */}
-        {/* {<WorkingMemory/>} */}
-        {/* {<MotorFunction/>} */}
-        {/* {<WordRecognition/>} */}
-        {/* <MainPage/> */}
-        <Tests setMainClass={setMainClass}/>
+          {ShowDigitVigilance ?
+            <DigitVigilance/>
+          : null}
+
+          {ShowMemoryScanning ?
+            <MemoryScanning/>
+          : null}
+        
+          {ShowMotorFunction ?
+            <MotorFunction/>
+          : null}
+
+          {ShowNumberVigilance ?
+            <NumberVigilance/>        
+          :null}
+
+          {ShowPictureRecognition ?
+            <PictureRecognition/>        
+          :null}
+
+          {ShowReactionTime ?
+            <ReactionTime/>        
+          :null}
+
+          {ShowVerbalLearning ?
+            <VerbalLearning/>        
+          :null}
+
+          {ShowWordRecognition ?
+            <WordRecognition/>        
+          :null}
+
+          {ShowWorkingMemory ?
+            <WorkingMemory/>        
+          :null}
+        </div>
       </div>
+
+
+      {ShowPopover ?
+        <div className="w-full h-full flex justify-center items-center absolute top-0 z-2 bg-black opacity-65">
+        </div>
+      : null}
+
+      {ShowPopover ?
+        <div className="h-[50%] w-[30%] z-99 absolute top-[20%] left-[35%] bg-blue-400 rounded-2xl text-white">
+          <div className="p-12 grid grid-auto-rows">
+            <div className="text-3xl">
+                {TestTitle}
+            </div>
+            <div className="mt-36 text-xl">
+                {PopoverMessage}
+            </div>
+            <div className="underline cursor-pointer mt-[10%]" onClick={take_test}>
+              Take Test
+            </div>
+            <div className="flex flex-col w-full mt-[30%] underline cursor-pointer" onClick={e => hide_popover()}>
+                Close
+            </div>
+          </div>
+        </div>
+      : null}
+
     </main>
+
   )
 }
--- cognitive_test/src/screens/Tests.tsx
+++ cognitive_test/src/screens/Tests.tsx
@@ -1,95 +1,83 @@
 'use client'
 import React, {useEffect, useRef} from 'react';
-import {Button} from "@nextui-org/react"
-import { v4 as uuidv4 } from 'uuid';
-import Login from '@/components/Login';
-import Signup from '@/components/Signup';
 
 
 export default function Tests(props: any) {
-    const [ShowPopover, setShowPopover] = React.useState(false)
-    const [TestTitle, setTestTitle] = React.useState("")
-    const [PopoverMessage, setPopoverMessage] = React.useState("")
-
-    const main_class = ["h-full relative", "h-[100%] w-[100%] bg-blue-400 relative"]
-    const [MainClass, setMainClass] = React.useState(main_class[0])
-
-    useEffect(() => {
-
-    }, [])
-
-
-    function hide_popover(){
-        setShowPopover(false)
-        setMainClass(main_class[0])
-        setPopoverMessage("")
-        setTestTitle("")
-    }
-
 
     function show_info(test: any){
-        setMainClass(main_class[1])
-        setShowPopover(true)
+        props.setMainClass(props.main_class[1])
+        props.setShowPopover(true)
 
         switch (test){
+
             //CHOICE REACTION
             case 1:
-                setPopoverMessage("Either the word Yes or the word No is presented in the center of the screen. The user is presented a simple question and has to press the button corresponding to the answer as quickly as possible. There are 20 trials and the intertrial interval varies randomly between 1 and 2.5 seconds.")
-                setTestTitle("CHOICE REACTION TIME") 
+                props.setPopoverMessage("Either the word Yes or the word No is presented in the center of the screen. The user is presented a simple question and has to press the button corresponding to the answer as quickly as possible. There are 20 trials and the intertrial interval varies randomly between 1 and 2.5 seconds.")
+                props.setTestTitle("CHOICE REACTION TIME") 
+                props.setTestID(test)
                 break
 
             //DIGIT VIGILANCE
             case 2:
-                setPopoverMessage("Players are asked to find two specified numbers, which appear randomly within fifty rows of fifty single digits. The goal is to find as many of the numbers as possible.")
-                setTestTitle("DIGIT VIGILANCE")
+                props.setPopoverMessage("Players are asked to find two specified numbers, which appear randomly within fifty rows of fifty single digits. The goal is to find as many of the numbers as possible.")
+                props.setTestTitle("DIGIT VIGILANCE")
+                props.setTestID(test)
                 break
 
             //MEMORY SCANNING
             case 3:
-                setPopoverMessage("Three digits are presented singly at the rate of one every 2.5 seconds for the player to remember. A series of 18 digits is then presented. For each, the player must press Yes or No according to whether the digit is thought to be one of the three presented initially.")
-                setTestTitle("MEMORY SCANNING")
+                props.setPopoverMessage("Three digits are presented singly at the rate of one every 2.5 seconds for the player to remember. A series of 18 digits is then presented. For each, the player must press Yes or No according to whether the digit is thought to be one of the three presented initially.")
+                props.setTestTitle("MEMORY SCANNING")
+                props.setTestID(test)
                 break
 
             //MOTOR FUNCTION
             case 4:
-                setPopoverMessage("A series of individual shapes appear randomly on the screen. The goal is to click each shape as fast as possible.")
-                setTestTitle("MOTOR FUNCTION")
+                props.setPopoverMessage("A series of individual shapes appear randomly on the screen. The goal is to click each shape as fast as possible.")
+                props.setTestTitle("MOTOR FUNCTION")
+                props.setTestID(test)
                 break
 
             //NUMBER VIGILANCE
             case 5:
-                setPopoverMessage("A number appears at the top of the screen. When the test is started, random numbers are shown a quick rate for 3 minutes. Click the 'Okay' button when the two numbers match to test your reaction time.")
-                setTestTitle("NUMBER VIGILANCE")
+                props.setPopoverMessage("A number appears at the top of the screen. When the test is started, random numbers are shown a quick rate for 3 minutes. Click the 'Okay' button when the two numbers match to test your reaction time.")
+                props.setTestTitle("NUMBER VIGILANCE")
+                props.setTestID(test)
                 break
 
             //PICTURE RECOGNITION
             case 6:
-                setPopoverMessage("Fourteen pictures are displayed, one every 1.5 seconds. The player is told to memorize each picture. Afterward, fourteen more pictures are shown. This time the picture set only contains some of the items from the original display. The player is asked if each displayed picture from the second set was in the original set.")
-                setTestTitle("PICTURE RECOGNITION")
+                props.setPopoverMessage("Fourteen pictures are displayed, one every 1.5 seconds. The player is told to memorize each picture. Afterward, fourteen more pictures are shown. This time the picture set only contains some of the items from the original display. The player is asked if each displayed picture from the second set was in the original set.")
+                props.setTestTitle("PICTURE RECOGNITION")
+                props.setTestID(test)
                 break
 
             //REACTION TIME
             case 7:
-                setPopoverMessage("A button appears at the center of the screen. The user is intended to press it as quickly as possible. There are twenty trials and the button appears randomly between .5 and 2.5 seconds.")
-                setTestTitle("REACTION TIME")
+                props.setPopoverMessage("A button appears at the center of the screen. The user is intended to press it as quickly as possible. There are twenty trials and the button appears randomly between .5 and 2.5 seconds.")
+                props.setTestTitle("REACTION TIME")
+                props.setTestID(test)
                 break
 
             //VERBAL LEARNING
             case 8:
-                setPopoverMessage("A series of twelve words will be shown on the screen for three seconds each. Afterward, twelve words will be shown again. While each is shown, decide if it was in the original sequence.")
-                setTestTitle("VERBAL LEARNING")
+                props.setPopoverMessage("A series of twelve words will be shown on the screen for three seconds each. Afterward, twelve words will be shown again. While each is shown, decide if it was in the original sequence.")
+                props.setTestTitle("VERBAL LEARNING")
+                props.setTestID(test)
                 break
 
             //WORD RECOGNITION
             case 9:
-                setPopoverMessage("Ten words are displayed, one every 1.5 seconds. The player is told to memorize each word. Afterward, ten more words are shown. This time the word set only contains some of the items from the original display. The player is asked if each word from the second set was in the original set.")
-                setTestTitle("WORD RECOGNITION")
+                props.setPopoverMessage("Ten words are displayed, one every 1.5 seconds. The player is told to memorize each word. Afterward, ten more words are shown. This time the word set only contains some of the items from the original display. The player is asked if each word from the second set was in the original set.")
+                props.setTestTitle("WORD RECOGNITION")
+                props.setTestID(test)
                 break
 
             //WORKING MEMORY
             case 10:
-                setPopoverMessage("The test begins with three colored boxes shown on the screen. One box contains a token. When the token is found, it is added to the 'found list' above the play area. The token is then moved to another box for the user to find again. The rate at which this occurs is equal to the amount boxes shown on the screen.")
-                setTestTitle("SPATIAL WORKING MEMORY")
+                props.setPopoverMessage("The test begins with three colored boxes shown on the screen. One box contains a token. When the token is found, it is added to the 'found list' above the play area. The token is then moved to another box for the user to find again. The rate at which this occurs is equal to the amount boxes shown on the screen.")
+                props.setTestTitle("SPATIAL WORKING MEMORY")
+                props.setTestID(test)
                 break
 
         }
@@ -98,15 +86,14 @@ export default function Tests(props: any) {
 
 
   return(
-    <div className={MainClass}>
+    <div>
         <div className="row">
             FLOURISH SCIENCE COGNITIVE TESTS
         </div>
         <div className="row mt-12 text-sky-400">
             A list of the various cognitive tests. Click the test name to show its description. Click the 'Start Test' button below a given test name to start the test. 
         </div>
-
-        <div className="grid grid-auto-rows place-items-center auto-rows">
+        <div className="grid grid-auto-rows place-items-center auto-rows mt-24">
             <div>
                 <div className="text-green-400 mt-12 cursor-pointer underline" onClick={e => show_info(1)}>
                     Choice Reaction Time
@@ -158,24 +145,7 @@ export default function Tests(props: any) {
                 </div>
             </div>
         </div>
-
-        {ShowPopover ?
-            <div className="h-[50%] w-[50%] z-99 sticky top-0 bottom-0 left-0 right-0">
-                <div>
-                    {TestTitle}
-                </div>
-                <div>
-                    {PopoverMessage}
-                </div>
-                <div className="mt-12" onClick={e => hide_popover()}>
-                    Close
-                </div>
-            </div>
-        : null}
-    
     </div>
-    
-
   )
 
 }
